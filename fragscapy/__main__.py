c0f05c0697d6ffeab97dd7fb5ded46863e1a03aa
Maël Kervella <dev@maelkervella.eu>
2019-05-28 11:48:41 +0200

    cleanup pylint

--- fragscapy/__main__.py
+++ fragscapy/__main__.py
@@ -1,2 +1,9 @@
+"""
+Main module for starting fragscapy package as a simple command
+`python fragscapy ...`
+instead of something clusmy as
+`python fragscapy/__main__.py`
+"""
+
 from fragscapy.commandline import command
 command()
--- fragscapy/commandline.py
+++ fragscapy/commandline.py
@@ -1,7 +1,10 @@
+"""
+Handle everything related to the command line and its many options. The main
+entry point is `command()` which will parse the arguments and triggers the
+correct function depending on the arguments given.
+"""
 import argparse
 import logging
-import importlib
-import os
 import traceback
 
 from scapy.config import conf
@@ -11,13 +14,21 @@ from fragscapy.config import Config
 from fragscapy.engine import Engine
 from fragscapy.modgenerator import get_all_mods, get_mod
 
-PROG_NAME="Fragscapy"
+PROG_NAME = "Fragscapy"
 DESCRIPTION = ("Runs a series of tests on the network and modify the packets "
                "on the fly in order to test the behavior of the machines on "
                "the network")
 EPILOG = "Fragscapy {version} - {author}".format(version=version, author=author)
 
 def command():
+    """
+    Parse the arguments passed to the command line and triggers the correct
+    function. The main sub commands are:
+    * 'list' for listing the mods that can be detected
+    * 'usage' for detailling the usage of one (or multiple) mods
+    * 'checkconfig' to check various aspects of a configuration file
+    * 'start' to run the test suite described by a config file
+    """
     parser = argparse.ArgumentParser(
         description=DESCRIPTION, epilog=EPILOG, prog=PROG_NAME
     )
@@ -31,7 +42,7 @@ def command():
     subparsers = parser.add_subparsers(dest='subcmd')
 
     # fragscapy list
-    parser_list = subparsers.add_parser('list', help="List the available mods")
+    subparsers.add_parser('list', help="List the available mods")
 
     # fragscapy usage
     parser_usage = subparsers.add_parser('usage', help="Details the usage of a mod")
@@ -122,6 +133,7 @@ def command():
 
 
 def list_mods():
+    """ List all the mods that can be detected. """
     all_mods_name = sorted(map(
         lambda x: x.name or x.__class__.__name__.lower(),
         get_all_mods()
@@ -132,6 +144,7 @@ def list_mods():
 
 
 def start(args):
+    """ Run the test suite. """
     if not args.scapy_output:
         # Removes warning messages
         logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
@@ -145,6 +158,7 @@ def start(args):
 
 
 def usage(args):
+    """ Print the usage for specific mods. """
     for mod_name in args.mod:
         try:
             mod = get_mod(mod_name)
@@ -155,6 +169,11 @@ def usage(args):
 
 
 def checkconfig(args):
+    """
+    Checks that the config file looks correct. It does not guarantee that
+    there will be no crash during the test suite but it tries to catch
+    everything before running it.
+    """
     try:
         print(">>> Loading config file")
         config = Config(args.config_file)
@@ -163,15 +182,17 @@ def checkconfig(args):
         engine = Engine(config, progressbar=(not args.no_progressbar), **kwargs)
         print(">>> Checking Netfilter rules")
         engine.check_nfrules()
-        print(">>> Checking mod list generation")
+        print(">>> Checking mod list generation (output to '{}')"
+              .format(args.modif_file))
         engine.check_modlist_generation()
-    except Exception as e:
+    except BaseException as e:  # pylint: disable=broad-except
         if args.traceback:
             traceback.print_tb(e.__traceback__)
         print("{name}: {msg}".format(name=e.__class__.__name__, msg=e))
 
 
 def _filter_kwargs(args, keys):
+    """ Filter and transforme argparse's args to a kwargs. """
     kwargs = dict()
     for k in keys:
         if hasattr(args, k) and getattr(args, k) is not None:
