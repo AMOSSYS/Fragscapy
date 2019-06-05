532e99bfee74df0336d859e06c38830d6f8d83c4
Maël Kervella <dev@maelkervella.eu>
2019-06-05 14:46:26 +0200

    Add Overlap modification

--- fragscapy/modifications/fragment6.py
+++ fragscapy/modifications/fragment6.py
@@ -1,72 +1,8 @@
 """Fragments the IPv6 packets at the L3-layer."""
 
-import scapy.layers.inet6
-import scapy.packet
-
 from fragscapy.modifications.mod import Mod
+from fragscapy.modifications.utils import fragment6
 from fragscapy.packetlist import PacketList
-
-
-PROCESS_HEADERS = ("IPv6", "IPv6ExtHdrHopByHop", "IPv6ExtHdrRouting")
-
-
-def name(layer):
-    """Returns the class name of a protocol layer."""
-    return layer.__class__.__name__
-
-
-def get_per_frag_hdr(pkt):
-    """Returns the last 'Scapy layer' of the "Per-Fragment Headers" part of
-    the packet.
-
-    The "Per-Fragment Headers" part is the chain of IPv6 headers that is
-    repeated for every fragment because they are useful for routing and
-    defragmenting.
-
-    Args:
-        pkt: The Scapy packet to examine.
-
-    Returns:
-        A reference to the last 'Scapy layer' of the "Per-Fragment Headers".
-
-    Examples:
-        >>> get_per_frag_hdr(IPv6()/IPv6ExtHdrRouting()/AH()/TCP()/"PLOP")
-        <IPv6ExtHdrRouting  nh=AH Header |<AH  |<TCP  |<Raw  load='PLOP' |>>>>
-    """
-    current = pkt
-    ret = current
-    while current is not scapy.packet.NoPayload():
-        if name(current) in PROCESS_HEADERS:
-            ret = current
-        current = current.payload
-    return ret
-
-
-def insert_frag_hdr(pkt):
-    """Inserts a "Fragment Extension Header" in a packet just after the
-    "Per-Fragment Headers" part.
-
-    Args:
-        pkt: The packet to modify.
-
-    Returns:
-        The same packet with a well-placed Fragment Extension Header.
-
-    Examples:
-        >>> insert_frag_hdr(IPv6()/IPv6ExtHdrRouting()/AH()/TCP()/"PLOP")
-        <IPv6  nh=Routing Header |
-          <IPv6ExtHdrRouting  nh=Fragment Header |
-            <IPv6ExtHdrFragment  nh=AH Header |
-              <AH |
-                <TCP  |
-                  <Raw  load='PLOP' |>>>>>>
-    """
-    current = get_per_frag_hdr(pkt)
-    current.payload = (
-        scapy.layers.inet6.IPv6ExtHdrFragment(nh=current.nh)
-        / current.payload
-    )
-    return pkt
 
 
 class Fragment6(Mod):
@@ -109,8 +45,7 @@ class Fragment6(Mod):
 
         for pkt in pkt_list:
             if pkt.pkt.haslayer('IPv6'):
-                mod_pkt = insert_frag_hdr(pkt.pkt)
-                fragments = scapy.layers.inet6.fragment6(mod_pkt, self.fragsize)
+                fragments = fragment6(pkt.pkt, self.fragsize)
 
                 index = len(new_pl) - 1
                 for fragment in fragments:
